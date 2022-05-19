4d4151e32a8cc3402f3c2efd18f31c27c9bf1093
Daniel Larsen <dalars@microsoft.com>
2022-05-19 21:34:45 +1200

    Batch send

--- ServiceBusThroughput/Program.cs
+++ ServiceBusThroughput/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
 using System.Diagnostics;
-using System.Threading.Tasks;
-using Azure.Messaging.ServiceBus;
-using Azure.Messaging.ServiceBus.Administration;
 
 class Program
 {
@@ -12,16 +9,40 @@ class Program
         string connectionString = null;
         string topicName = "throughput2";
         int numOfMessages = 100;
+        int batchSize = 2;
 
         if (args.Length == 0) throw new ArgumentException("First argument must be Service Bus connection string");
 
         connectionString = args[0];
         if (args.Length > 1) topicName = args[1];
         if (args.Length > 2) int.TryParse(args[2], out numOfMessages);
+        if (args.Length > 3) int.TryParse(args[3], out batchSize);
 
         var client = new ServiceBusClient(connectionString);
-        var sender = client.CreateSender(topicName);
+        ServiceBusSender sender = client.CreateSender(topicName);
 
+        await NoBatch(sender, numOfMessages);
+        await Batch(sender, numOfMessages, batchSize);
+
+        await sender.DisposeAsync();
+        await client.DisposeAsync();
+    }
+
+    static private string DataBytes(int length)
+    {
+        // Construct a random string of data
+        string data = "";
+        var random = new Random();
+        for (int i = 0; i < length; i++)
+        {
+            data += (char)random.Next(65, 90);
+        }
+
+        return data;
+    }
+
+    static async Task NoBatch(ServiceBusSender sender, int numOfMessages)
+    {
         string data = DataBytes(4096);
         var stopwatch = new Stopwatch();
         int count = 0;
@@ -42,60 +63,59 @@ class Program
             Console.WriteLine();
             stopwatch.Stop();
 
-            await sender.DisposeAsync();
-            await client.DisposeAsync();
         }
 
         Console.WriteLine($"{count} messages sent in {stopwatch.ElapsedMilliseconds} ms equals throughput of {count / stopwatch.Elapsed.TotalSeconds} per second");
-
-        Console.WriteLine("Press any key to end the application");
-        Console.ReadKey();
     }
 
-
-    static private string DataBytes(int length)
+    static private async Task Batch(ServiceBusSender sender, int numOfMessages, int batchSize)
     {
-        // Construct a random string of data
-        string data = "";
-        var random = new Random();
-        for (int i = 0; i < length; i++)
+        // create a batch 
+        ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+        string data = DataBytes(4096);
+        var stopwatch = new Stopwatch();
+        int count = 0;
+        int batchCount = 0;
+
+        try
         {
-            data += (char)random.Next(65, 90);
+            stopwatch.Start();
+
+            for (int i = 0; i < numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage(data)))
+                {
+                    throw new Exception("Batch full.");
+                }
+
+                batchCount++;
+                count++;
+
+                if (batchCount >= batchSize)
+                {
+                    await sender.SendMessagesAsync(messageBatch);
+                    messageBatch.Dispose();
+                    messageBatch = await sender.CreateMessageBatchAsync();
+                    batchCount = 0;
+                    Console.Write($"{count} ");
+                }
+            }
+
+            if (batchCount > 0)
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                messageBatch.Dispose();
+            }
+        }
+        finally
+        {
+            Console.WriteLine();
+            stopwatch.Stop();
         }
 
-        return data;
+        Console.WriteLine($"{count} messages in batches of {batchSize} sent in {stopwatch.ElapsedMilliseconds} ms equals throughput of {count / stopwatch.Elapsed.TotalSeconds} per second");
     }
-
-    //static private async Task Batch(int numOfMessages)
-    //{
-    //    // create a batch 
-    //    using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
-
-    //    for (int i = 1; i <= numOfMessages; i++)
-    //    {
-    //        // try adding a message to the batch
-    //        if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
-    //        {
-    //            // if it is too large for the batch
-    //            throw new Exception($"The message {i} is too large to fit in the batch.");
-    //        }
-    //    }
-
-    //    try
-    //    {
-    //        // Use the producer client to send the batch of messages to the Service Bus topic
-    //        await sender.SendMessagesAsync(messageBatch);
-    //        Console.WriteLine($"A batch of {numOfMessages} messages has been published to the topic.");
-    //    }
-    //    finally
-    //    {
-    //        // Calling DisposeAsync on client types is required to ensure that network
-    //        // resources and other unmanaged objects are properly cleaned up.
-    //        await sender.DisposeAsync();
-    //        await client.DisposeAsync();
-    //    }
-
-    //}
-
 }
 
